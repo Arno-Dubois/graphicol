9f254a784200be42be711dca7e39aa71348ec5a1
Arno-Dubois <144135021+Arno-Dubois@users.noreply.github.com>
2025-02-18 09:10:03 +0100

    Start jsonLoader

--- model/src/main/java/com/graphicol/graph/Model.java
+++ model/src/main/java/com/graphicol/graph/Model.java
@@ -1,4 +1,9 @@
 package com.graphicol.graph;
 
 public class Model implements IModel {
+    private final IGraph<INode<String>> graph = new Graph<>();
+
+    public Model() {
+        this.graph.loadFromJson("AventuriersDuRailEurope.json");
+    }
 }
