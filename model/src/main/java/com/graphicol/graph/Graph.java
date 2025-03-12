970e714df46eadd8e627cdbce446b297d06a99c2
Arno-Dubois <144135021+Arno-Dubois@users.noreply.github.com>
2025-03-12 11:39:01 +0100

    Refactor getMinLinkFromTreeNode

--- model/src/main/java/com/graphicol/graph/Graph.java
+++ model/src/main/java/com/graphicol/graph/Graph.java
@@ -2,6 +2,7 @@ package com.graphicol.graph;
 
 import fr.arnodubois.treenode.NaryTreeNode;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,22 +13,28 @@ public abstract class Graph<E> implements IGraph<E> {
     @Override
     public NaryTreeNode<INode<E>> getSpanningTreeByPrim() {
         if (this.getNodes().isEmpty()) return null;
-        NaryTreeNode<INode<E>> spanningTree = new NaryTreeNode<>(this.get(10));
+        NaryTreeNode<INode<E>> spanningTree = new NaryTreeNode<>(this.get(0));
+
         while (spanningTree.getSize() != this.getNodes().size()) {
-            ILink<E> minLink = null;
-            for (INode<E> node : spanningTree.toByWidthList()) {
-                for (ILink<E> link : node.getLinks()) {
-                    if (!spanningTree.contains(link.getEnd())) {
-                        minLink = (minLink == null ? link : (minLink.compareTo(link) > 0 ? link : minLink));
-                    }
-                }
-            }
+            ILink<E> minLink = Graph.getMinLinkFromTreeNode(spanningTree);
+
             if (minLink != null) {
                 spanningTree.getNodeFromElement(minLink.getStart()).addChild(minLink.getEnd());
-
             }
         }
         return spanningTree;
+    }
+
+    private static <E> @Nullable ILink<E> getMinLinkFromTreeNode(final NaryTreeNode<INode<E>> treeNode) {
+        ILink<E> minLink = null;
+        for (INode<E> node : treeNode.toByWidthList()) {
+            for (ILink<E> link : node.getLinks()) {
+                if (!treeNode.contains(link.getEnd())) {
+                    minLink = (minLink == null ? link : (minLink.compareTo(link) > 0 ? link : minLink));
+                }
+            }
+        }
+        return minLink;
     }
 
     @Override
